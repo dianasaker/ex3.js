1a981d87bb1b7a0b47591ed1501380ff85adf592
Diana Saker <Diana Saker>
2016-08-01 17:17:12 +0300

    ex3.js

---
+++ script.js
@@ -0,0 +1,13 @@
+var arr=[2,4,6,8,10,12,14,16,18,20],i;
+console.log(arr.length);
+console.log(arr[1],arr[3],arr[6]);
+arr.indexof(10);
+arr.splice(8,2);
+var arr1=[1,"one",8,"eight",5,"five",7,"siven",9,"nine"];
+var arr2=[student1={name:"Diana",eyescolor:"blue",height:"169"},student2={name:"aya",eyescolor:"brown",height:"170"},student3={name:"mazen",eyescolor:"brown",height:"175"}];
+arr2.shift();
+for(i=0;i<arr2.length;i++) {
+	console.log(arr2[i]);
+}
+arr2.unshift(lecturer={name:"Dotan",eyescolor:"brown",height:"175"});
+arr2[1].eyescolor=face{eyes:color="blue"}; //i tried to search for the right way to fix it but i didn`t find
